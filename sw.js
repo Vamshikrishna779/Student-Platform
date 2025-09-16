b42ad0db6c8048631be847c6c12fa91887bb4869
Vamshikrishna779 <vamshikrishna9608@gmail.com>
2025-09-16 08:34:52 +0530

    Updates

--- sw.js
+++ sw.js
@@ -1,14 +1,1 @@
-const CACHE_NAME = "studiq-cache-v1";
-const urlsToCache = ["/Student-Platform/", "/Student-Platform/index.html"];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
-  );
-});
+if(!self.define){let e,s={};const i=(i,n)=>(i=new URL(i+".js",n).href,s[i]||new Promise(s=>{if("document"in self){const e=document.createElement("script");e.src=i,e.onload=s,document.head.appendChild(e)}else e=i,importScripts(i),s()}).then(()=>{let e=s[i];if(!e)throw new Error(`Module ${i} didn’t register its module`);return e}));self.define=(n,r)=>{const t=e||("document"in self?document.currentScript.src:"")||location.href;if(s[t])return;let o={};const l=e=>i(e,t),d={module:{uri:t},exports:o,require:l};s[t]=Promise.all(n.map(e=>d[e]||l(e))).then(e=>(r(...e),o))}}define(["./workbox-5ffe50d4"],function(e){"use strict";self.skipWaiting(),e.clientsClaim(),e.precacheAndRoute([{url:"404.html",revision:"188f2fbd9d02497d24e3d52d4c7c9512"},{url:"assets/index-B_Uyatqx.css",revision:null},{url:"assets/index-CYKjcf7U.js",revision:null},{url:"assets/workbox-window.prod.es5-CwtvwXb3.js",revision:null},{url:"env.js",revision:"1c1326434f536c559a96a6ec0a3920e7"},{url:"index.html",revision:"4cb12809d6a52c96b58074c3f92b358a"},{url:"offline.html",revision:"fdac23414533823e28f9b79d8234fe5c"},{url:"manifest.webmanifest",revision:"41914fb130db6606d0696d0d817f7e3a"}],{}),e.cleanupOutdatedCaches(),e.registerRoute(new e.NavigationRoute(e.createHandlerBoundToURL("index.html")))});
